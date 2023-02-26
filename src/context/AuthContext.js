51d92c4769a83576dd69a4cf3a91b36b66c78fa0
MariusSolheim <marius.solheim@noroff.no>
2023-02-26 20:54:28 +0100

    First commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,20 @@
+//Source: Noroff
+
+import React, { useState } from "react";
+import setAuthOnLoad from "./auth";
+
+const AuthContext = React.createContext();
+
+export const AuthProvider = (props) => {
+  const [auth, setAuth] = useState(setAuthOnLoad);
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
+/*
+
+*/
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Colours } from "../global-styles";
+/*
+const ThemeContext = React.createContext();
+
+export const Theme = ({ children }) => {
+  return (
+    <GlobalStateContext.Provider value={Colours}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
+
+export default GlobalStateContext;
+*/
